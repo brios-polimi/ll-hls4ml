60003aa79b7e445611faca0554694bd204ebd9b2
brios-polimi <brendanmichael.rios@mail.polimi.it>
2026-06-04 19:59:06 +0200

    Temporary subset run of transductive vs inductive'

--- src/ll_hls4ml/training/loops.py
+++ src/ll_hls4ml/training/loops.py
@@ -280,7 +280,7 @@ def transductive_vs_inductive_fit(
     use_cuda = device_type.startswith("cuda") and torch.cuda.is_available()
 
     training_histories = {}
-    for kernel_type in kernel_types:
+    for kernel_type in kernel_types if kernel_type in ["dense_resource", "exemplar", "rule4ml"]:
         model = None
         loop_optimizer = None
         train_loader = val_loader = test_loader = None
